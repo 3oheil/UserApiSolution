eb9855d4542e1bd495d286240b06b49e9cd85afa
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-10-02 03:15:09 -0800

    create user Model

---
+++ UserApiSolution/UserApi/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UserApi.Models
+{
+    public class User : BaseModel
+    {
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; } = "User";
+    }
+}
