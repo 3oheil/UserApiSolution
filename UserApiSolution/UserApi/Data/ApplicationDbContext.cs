df98b8cb7a99df9b8b02865a3f7d2e6fdea43788
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-10-02 03:14:59 -0800

    create DbContext

---
+++ UserApiSolution/UserApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserApi.Models;
+using FluentValidation;
+
+namespace UserApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
