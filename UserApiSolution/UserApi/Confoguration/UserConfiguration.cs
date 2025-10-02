cc9920bd4299c69c6fb5cee106b8a50757ae77d4
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-10-02 03:14:47 -0800

    fluent Validation

---
+++ UserApiSolution/UserApi/Confoguration/UserConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserApi.Models;
+
+namespace UserApi.Confoguration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            
+        }
+    }
+}
